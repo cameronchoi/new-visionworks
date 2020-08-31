8032dc581458e503c6fd103352f5be0fe61cc6cb
Cameron Choi <cameron.choi99@gmail.com>
2020-08-31 18:26:33 +1000

    Create under construction page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './visionworks.svg'
+import './App.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <br />
+        <p>This website is currently under construction</p>
+        <p>현재 업데이트 중입니다</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
